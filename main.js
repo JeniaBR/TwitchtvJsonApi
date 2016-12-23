388d29da6558dbb8ef6d333f14677cb8ac1f1cc1
Jenia Brook <jeniabrook@gmail.com>
2016-12-23 17:19:14 +0200

    Added the ability to see the status of fcc

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function checkFCCIsOnline() {
+    $.ajax({
+        type: "GET",
+        url: "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp",
+        dataType: "json",
+        success: function (data) {
+            if (data.stream === null) {
+                $('#fcc-status').html("Free Code Camp is OFFLINE");
+            }else{
+                $('#fcc-status').html("Free Code Camp is ONLINE");                
+            }
+        }
+    });
+}
+
+
 $(document).ready(function () {
-    
+    checkFCCIsOnline();
 });
