1e41051e6974974bb341784fa351e11d74561c85
Jenia Brook <jeniabrook@gmail.com>
2016-12-27 22:35:15 +0200

    Add new logic for online, offline, not exist users Add new styling

--- main.js
+++ main.js
@@ -19,27 +19,33 @@ function getStreamInfo(user) {
 function showData(channelInfo, streamInfo) {
     console.log(streamInfo);
 
-    var logo, displayName, status;
+    var logo, displayName, onlineUserStatus, userConnectivity;
+
+    streamInfo[0].stream != null ? userConnectivity = "online" : userConnectivity = "offline";
 
     logo = channelInfo[0].logo;
     displayName = channelInfo[0].display_name;
-    status = channelInfo[0].status;
+    userConnectivity === 'online' ? onlineUserStatus = channelInfo[0].status : onlineUserStatus = "Offline";
 
     if (channelInfo[0].status === 404) {
-        logo = "https://dinolover1314.files.wordpress.com/2012/07/pagenotfound_icon.png"
+        logo = "https://dinolover1314.files.wordpress.com/2012/07/pagenotfound_icon.png";
         displayName = channelInfo[0].error;
-        status = channelInfo[0].message;
+        onlineUserStatus = channelInfo[0].message;
+        userConnectivity = "not-exist"
     }
 
-    var item = '<div class = "col-xs-4"><img src="' + logo + '"class="img-responsive img-circle" width="150" height="150"></div>';
+    var item = '<div class = "col-xs-4"><img src="' + logo + '"class="img-responsive img-circle" width="100" height="100"></div>';
     item += '<div class = "col-xs-4">' + displayName + '</div>';
-    item += '<div class = "col-xs-4">' + status + '</div>';
+    item += '<div class = "col-xs-4 user-status">' + onlineUserStatus + '</div>';
 
-    $('#app-container').append('<div class="row">' + item + '</div>');
+    var element = '<div class="row '+ userConnectivity +'" style="display: none;">' + item + '</div>';
+
+    userConnectivity === 'online' ? $('#fcc-users').prepend(element) : $('#fcc-users').append(element);
+    $('.row').slideDown('fast');
 
 }
 
-function getFccFollowers(users) {
+function getDataFromTwitchAPI(users) {
 
     for (let i = 0; i < users.length; i++) {
         $.when(getChannelInfo(users[i]), getStreamInfo(users[i])).then(showData);
@@ -50,6 +56,6 @@ function getFccFollowers(users) {
 
 $(document).ready(function () {
     var twitchUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
-    getFccFollowers(twitchUsers);
+    getDataFromTwitchAPI(twitchUsers);
 
 });
